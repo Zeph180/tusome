e470144ea76fdd8cd87f2d85ed345248c7e26524
Zeph180 <zephrichards1@gmail.com>
2022-05-30 02:45:52 +0300

    created a card component and used it in a flat list on the dashboard

---
+++ src/constants/Card.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import RoundAvatar from "./RoundAvatar";
+
+
+export default function Card({ onPress, cardTitle, imageUrl }) {
+	return (
+		<TouchableOpacity onPress={onPress} style={styles.container}>
+			<RoundAvatar 
+				imageUrl={imageUrl}
+				style={styles.cardImage}
+			/>
+			<Text style={styles.cardTitle}>{cardTitle}</Text>
+		</TouchableOpacity>
+	);
+}
+
+
+Card.propTypes = {
+	onPress: PropTypes.func.isRequired,
+	cardTitle: PropTypes.string.isRequired,
+	imageUrl: PropTypes.string,
+	style: PropTypes.object,
+};
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: "rgba(245, 246, 252, 1)",
+		width: 160,
+		height: 132,
+		justifyContent: "center",
+		alignItems: "center",
+		borderRadius: 10,
+        margin: 10
+	},
+	cardImage: {
+		width: 64,
+		height: 64,
+		backfaceVisibility: "visible",
+		backgroundColor: "pink"
+	},
+	cardTitle: {
+		fontSize: 16,
+		marginTop: 10,
+	}
+});

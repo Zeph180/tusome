4f2c00c8c44f8851531de0b176bc0836a40d9ba7
Zeph180 <zephrichards1@gmail.com>
2022-05-31 01:31:27 +0300

    odified Round avatar to accept onpress prop

--- src/constants/RoundAvatar.js
+++ src/constants/RoundAvatar.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
+import {  } from "react-native-web";
 
-export default function RoundAvatar({ style, imageUrl }) {
+export default function RoundAvatar({ style, imageUrl, handleImagePress }) {
 	return (
-		<View>
+		<TouchableOpacity 
+			onPress={handleImagePress}>
 			<Image 
+				onp
 				style={style}
 				source={{
 					uri:imageUrl,
 				}}
 			/>
-		</View>
+		</TouchableOpacity>
 	);
 }

bd22d9c6579e4ec120242f22fd6a5ddf516b488a
Zeph180 <zephrichards1@gmail.com>
2022-06-04 02:10:06 +0300

    New screens

---
+++ src/screens/DiscussionsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function DiscussionsScreen(params) {
+	return (
+		<View style={{justifyContent: "center", alignItems: "center" }}>
+			<Text>Coming Soon</Text>
+		</View>
+
+	);
+}
+

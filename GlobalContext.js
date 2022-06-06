e2eeb8b0b34f36763cb3199da71d86c116f73f16
Zeph180 <zephrichards1@gmail.com>
2022-06-07 01:51:12 +0300

    removed books.json

--- GlobalContext.js
+++ GlobalContext.js
@@ -1,7 +1,6 @@
 import React, { createContext, useState, useContext } from "react";
 import userData from "./dummy/user.json";
 import PropTypes from "prop-types";
-import userBooks from "./dummy/books.json"
 
 const AppContext = createContext();
 
@@ -9,10 +8,9 @@ export const useGlobalContext = () => useContext(AppContext);
 
 export default function GlobalContext({ children }) {
 	const [user, setUser] = useState(userData);
-	const [books, setBooks] = useState(userBooks);
 
 	return (
-		<AppContext.Provider value={{ user, setUser, books }}>
+		<AppContext.Provider value={{ user, setUser }}>
 			{children}
 		</AppContext.Provider>
 	);

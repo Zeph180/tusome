e87b018c29eb7db3df738d1f16dd8a9d1295b328
Zeph180 <zephrichards1@gmail.com>
2022-06-07 01:54:05 +0300

    modified

--- src/constants/BookCard.js
+++ src/constants/BookCard.js
@@ -1,87 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import CustomButton from "./CustomButton";
 
-
-
-// export default function BookCard() {
-    
-// 	return (
-// 		<View style={styles.container}>
-// 			<View>
-// 				<View style={styles.subjectContainer}>
-// 					<Text style={styles.subject}>European History</Text>
-// 				</View>
-// 				<Text style={styles.bookName}>Peacock</Text>
-// 				<Text style={styles.author}>Burrell Smith</Text>
-// 				<View style={styles.publisherContainer}>
-// 					<Text style={styles.publisher}>Mk Publishers</Text>
-// 					<Text style={styles.publisher}>1996</Text>
-// 				</View>
-// 				<View style={styles.btnView}> 
-// 					<CustomButton btnText="Read"/>
-// 				</View>
-// 			</View>
-// 		</View>
-// 	);
-// }
-
-
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		borderWidth: 3,
-// 		borderColor: "#E1E3E8",
-// 		borderRadius: 10,
-// 		height: 150,
-// 		margin: 10,
-// 		justifyContent: "center",
-// 	},
-// 	subject: {
-// 		color: "rgba(103, 137, 202, 1)",
-// 		paddingLeft:10,
-// 		fontSize: 15,
-// 		fontWeight: "bold"
-// 	},
-// 	subjectContainer: {
-// 		backgroundColor: "rgba(230, 239, 255, 1)",
-// 		width: 148,
-// 		height: 30,
-// 		borderRadius: 10,
-// 		justifyContent: "center",
-// 	},
-// 	bookName: {
-// 		marginLeft: 10,
-// 		fontWeight: "bold", 
-// 	},
-// 	author: {
-// 		marginLeft: 10, 
-// 	},
-// 	publisher: {
-// 		margin: 10
-// 	},
-// 	publisherContainer: {
-// 		flexDirection: "row",
-// 		justifyContent: "space-between"
-// 	},
-// 	btnView: {
-// 		alignItems: "center"
-// 	}
-// });
-
-export default function BookCard({ handleReadBtn }) {
+export function BookCard({ handleReadBtn, subjectName, bookTitle, publisher, edition, author }) {
 	return (
 		<View style={styles.container}>
-			<View>
+			<View style={{ marginLeft: 10 }}>
 				<View style={styles.subjectContainer}>
-					<Text style={styles.subject}>European History</Text>
+					<Text style={styles.subject}>{subjectName}</Text>
 				</View>
-				<Text style={styles.bookName}>Peacock</Text>
-				<Text style={styles.author}>Burrell Smith</Text>
+				<Text style={styles.bookName}>{bookTitle}</Text>
+				<Text style={styles.author}>{author}</Text>
 				<View style={styles.publisherContainer}>
-					<Text style={styles.publisher}>Mk Publishers</Text>
-					<Text style={styles.publisher}>1996</Text>
+					<Text style={styles.publisher}>{publisher}</Text>
+					<Text style={styles.publisher}>{edition}</Text>
 				</View>
 				<View style={styles.btnView}> 
 					<CustomButton 
@@ -94,9 +27,39 @@ export default function BookCard({ handleReadBtn }) {
 	);
 }
 
+export default function BooksList({ books }) {
+	
+	return (
+		<View>
+			{
+				books.map((book, i) => <BookCard 
+					subjectName={book.subjectName}
+					bookTitle={book.bookTitle}
+					publisher={book.publisher}
+					edition={book.edition}
+					author={book.author}
+					key={i}
+				/>)
+			}
+		</View>
+	);
+}
+
+
+
+
+BooksList.propTypes ={
+	books: PropTypes.array,
+};
+
 BookCard.propTypes = {
 	handleReadBtn: PropTypes.func,
-	user: PropTypes.object
+	user: PropTypes.object,
+	subjectName: PropTypes.string,
+	bookTitle: PropTypes.string,
+	publisher: PropTypes.string,
+	edition: PropTypes.string,
+	author: PropTypes.string
 };
 
 const styles = StyleSheet.create({
@@ -123,14 +86,8 @@ const styles = StyleSheet.create({
 		justifyContent: "center",
 	},
 	bookName: {
-		marginLeft: 10,
 		fontWeight: "bold", 
-	},
-	author: {
-		marginLeft: 10, 
-	},
-	publisher: {
-		margin: 10
+		marginTop: 8
 	},
 	publisherContainer: {
 		flexDirection: "row",
@@ -138,5 +95,6 @@ const styles = StyleSheet.create({
 	},
 	btnView: {
 		alignItems: "center",
+		marginVertical:10
 	}
 });
